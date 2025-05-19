55892ef1b9c908ed141252e4048955f02ce0542a
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-05-19 14:14:35 +0100

    remove commented-out methods from TaskRepository for cleaner code

--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -16,28 +16,52 @@ class TaskRepository extends ServiceEntityRepository
         parent::__construct($registry, Task::class);
     }
 
-//    /**
-//     * @return Task[] Returns an array of Task objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findAllOrderedByCreatedAt(): array
+    {
+        return $this->createQueryBuilder('t')
+            ->orderBy('t.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?Task
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    
+    public function findLatest(int $limit = 5): array
+    {
+        return $this->createQueryBuilder('t')
+            ->orderBy('t.createdAt', 'DESC')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
+    }
+
+   
+    public function searchByTerm(string $term): array
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.name LIKE :term OR t.description LIKE :term')
+            ->setParameter('term', '%' . $term . '%')
+            ->orderBy('t.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+
+    public function save(Task $task, bool $flush = true): void
+    {
+        $this->getEntityManager()->persist($task);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+
+    public function remove(Task $task, bool $flush = true): void
+    {
+        $this->getEntityManager()->remove($task);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
 }
